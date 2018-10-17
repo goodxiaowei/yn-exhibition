7b649c8495db4c1974debc6ce654d7df0b1e305f
goodxiaowei <871308180@qq.com>
2018-10-17 18:00:24 +0800

    Update VisiterRegMapper.java

--- src/main/java/com/yn/module/mapper/VisiterRegMapper.java
+++ src/main/java/com/yn/module/mapper/VisiterRegMapper.java
@@ -1,1 +1,17 @@
-package com.yn.module.mapper;import com.yn.module.entity.VisiterReg;/** * @Author: xw * @Description: * @Date: 12:44 2018/10/5 */public interface VisiterRegMapper {    public Integer insertVisiterReg(VisiterReg visiterReg);    public VisiterReg select(VisiterReg visiterReg);}
+package com.yn.module.mapper;
+
+import com.yn.module.entity.VisiterReg;
+
+/**
+ * @Author: xw
+ * @Description:
+ * @Date: 12:44 2018/10/5
+ */
+public interface VisiterRegMapper {
+
+    public Integer insertVisiterReg(VisiterReg visiterReg);
+
+    public VisiterReg select(VisiterReg visiterReg);
+
+    public String queryMaxNum();
+}
