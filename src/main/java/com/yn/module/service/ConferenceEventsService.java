36886b3695e9df16b2a208e6a01642df18a2d4f0
goodxiaowei <871308180@qq.com>
2018-10-22 14:12:48 +0800

    Update ConferenceEventsService.java

--- src/main/java/com/yn/module/service/ConferenceEventsService.java
+++ src/main/java/com/yn/module/service/ConferenceEventsService.java
@@ -1,1 +1,37 @@
-package com.yn.module.service;import com.github.pagehelper.PageInfo;import com.yn.common.msg.ResultResponse;import com.yn.module.entity.ConferenceEvents;import com.yn.module.mapper.ConferenceEventsMapper;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Service;import org.springframework.transaction.annotation.Transactional;import java.util.List;/** * @Author: xw * @Description: * @Date: 18:23 2018/9/26 */@Service@Transactional(readOnly = true)public class ConferenceEventsService {    @Autowired    private ConferenceEventsMapper conferenceEventsMapper;    public List<ConferenceEvents> getConferenceEventsList(ConferenceEvents conferenceEvents){        return conferenceEventsMapper.selectConferenceEventsList(conferenceEvents);    }    public Integer getConferenceEventsCount(ConferenceEvents conferenceEvents){        return conferenceEventsMapper.selectConferenceEventsCount(conferenceEvents);    }}
+package com.yn.module.service;
+
+import com.github.pagehelper.PageInfo;
+import com.yn.common.msg.ResultResponse;
+import com.yn.module.entity.ConferenceEvents;
+import com.yn.module.mapper.ConferenceEventsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+
+/**
+ * @Author: xw
+ * @Description:
+ * @Date: 18:23 2018/9/26
+ */
+@Service
+@Transactional(readOnly = true)
+public class ConferenceEventsService {
+
+    @Autowired
+    private ConferenceEventsMapper conferenceEventsMapper;
+
+    public List<ConferenceEvents> getConferenceEventsList(ConferenceEvents conferenceEvents){
+        return conferenceEventsMapper.selectConferenceEventsList(conferenceEvents);
+    }
+
+    public Integer getConferenceEventsCount(ConferenceEvents conferenceEvents){
+        return conferenceEventsMapper.selectConferenceEventsCount(conferenceEvents);
+    }
+
+    public ConferenceEvents getConferenceEvents(ConferenceEvents conferenceEvents){
+        return conferenceEventsMapper.selectConferenceEvents(conferenceEvents);
+    }
+}
