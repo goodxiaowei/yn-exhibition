587ea8e8b7d5ef2d940497015a2d22748a66da6f
goodxiaowei <871308180@qq.com>
2018-10-22 14:12:17 +0800

    Update ConferenceEventsMapper.java

--- src/main/java/com/yn/module/mapper/ConferenceEventsMapper.java
+++ src/main/java/com/yn/module/mapper/ConferenceEventsMapper.java
@@ -1,1 +1,19 @@
-package com.yn.module.mapper;import com.yn.module.entity.ConferenceEvents;import java.util.List;/** * @Author: xw * @Description: * @Date: 18:21 2018/9/26 */public interface ConferenceEventsMapper {    public List<ConferenceEvents> selectConferenceEventsList(ConferenceEvents conferenceEvents);    public Integer selectConferenceEventsCount(ConferenceEvents conferenceEvents);}
+package com.yn.module.mapper;
+
+import com.yn.module.entity.ConferenceEvents;
+
+import java.util.List;
+
+/**
+ * @Author: xw
+ * @Description:
+ * @Date: 18:21 2018/9/26
+ */
+public interface ConferenceEventsMapper {
+    public List<ConferenceEvents> selectConferenceEventsList(ConferenceEvents conferenceEvents);
+
+    public Integer selectConferenceEventsCount(ConferenceEvents conferenceEvents);
+
+    public ConferenceEvents selectConferenceEvents(ConferenceEvents conferenceEvents);
+
+}
